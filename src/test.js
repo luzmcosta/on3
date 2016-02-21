65929c837cb8a5e39240dcb4ae9203c20825e415
Luz M Costa <luzmcosta@gmail.com>
2016-02-21 15:03:37 -0500

    Sets dryrun property on tests.

--- src/test.js
+++ src/test.js
@@ -2,5 +2,6 @@ import on3 from './app.js';
 
 console.log('on3 ', on3);
 
-on3.increment({flag: '',gitmsg: "This is a test."}, () => {return;});
-on3.publish({gitmsg: "This is also a test."}, () => {return;});
+on3.increment({flag: '', gitmsg: 'This is a test.', dryrun: true}, () => {return;});
+
+on3.publish({gitmsg: 'This is also a test.', dryrun: true}, () => {return;});
