4415a51fe861d269c3ffe0cea0982550dfc20157
Luz M Costa <luzmcosta@gmail.com>
2016-02-21 16:04:53 -0500

    Updates bin/cli.js to include dryrun options.

--- bin/cli.js
+++ bin/cli.js
@@ -19,10 +19,10 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var cli = (0, _vorpal2.default)();
 
 // Updates the package version, tags the commit, pushes, and publishes.
-cli.command('publish', 'Publishes the package to GitHub and npm.').alias('p').option('-b, --branch [branch]', 'Set the branch to push to GitHub.').option('-g, --gittag [tag]', 'Tag the Git commit.').option('-m, --gitmsg [msg]', 'Set the Git commit message.').option('-n, --npmtag [tag]', 'Tag the node module.').option('-v, --version [version]', 'Increment to the given version.').action(_app2.default.publish);
+cli.command('publish', 'Publishes the package to GitHub and npm.').alias('p').option('-b, --branch [branch]', 'Set the branch to push to GitHub.').option('-g, --gittag [tag]', 'Tag the Git commit.').option('-m, --gitmsg [msg]', 'Set the Git commit message.').option('-n, --npmtag [tag]', 'Tag the node module.').option('-v, --version [version]', 'Increment to the given version.').option('--dryrun', 'Report the would-be changes without executing them.').action(_app2.default.publish);
 
 // Increment the node module's version.
-cli.command('increment [version]', 'Increment the npm version.').option('-m, --message [msg]', 'Sets the git commit message.').option('-f, --flag [flags]', 'Sets the flags on the `npm version` command.').option('-o, --only', 'Does not commit nor tag after updating the npm version.').option('-v, --version', 'Sets the version.').action(_app2.default.increment);
+cli.command('increment [version]', 'Increment the npm version.').option('-m, --message [msg]', 'Sets the git commit message.').option('-f, --flag [flags]', 'Sets the flags on the `npm version` command.').option('-o, --only', 'Does not commit nor tag after updating the npm version.').option('-v, --version', 'Sets the version.').option('--dryrun', 'Report the would-be changes without executing them.').action(_app2.default.increment);
 
 // Set the prompt.
 cli.delimiter('on >>').show();
