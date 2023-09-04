29cb1f331e310c7981629f3d6cf4dbc3c45c1664
Luz M. Costa <luzmcosta@gmail.com>
2023-09-03 23:09:50 -0400

    Draft tests for module:cli

---
+++ src/cli.test.js
@@ -0,0 +1,11 @@
+/**
+ * @file Test the CLI
+ * @module test:cli
+ */
+
+describe('cli', () => {
+  test.todo('can increment the node module\'s version')
+  test.todo('can update the package version, tag the commit, push, and publish')
+  test.todo('can get the node module\'s version')
+  test.todo('can get the current directory')
+})
